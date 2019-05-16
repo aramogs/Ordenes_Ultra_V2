1f5af3ecb71c13b5e8e4b85fe76b6d2fa0284cfd
aramogs <aramogs@users.noreply.github.com>
2019-05-16 16:40:24 -0600

    Prueba commit

--- routes.js
+++ routes.js
@@ -137,12 +137,13 @@ router.get('/ordenes', (req, res) => {
         id_orden = req.body.id_orden;
 
         db.query(`SELECT * FROM ordenes,areas_componentes_afectados 
-                  WHERE ordenes.id_orden = 780
+                  WHERE ordenes.id_orden = ${id_orden}
                   AND ordenes.parte_afectada = areas_componentes_afectados.id_componente`, function (err, result1, fields) {  
                        
          parteAfectada = result1[0].componente           
       res.render('cerrar_orden2.ejs', {
         data: {numeroEmpleado, nombreEmpleado,id_orden,parteAfectada}
+        //
       });
     });
   });
