860e696a9b68b84cd7b9362894d1cdf3327b06b0
aramogs <aramogs@users.noreply.github.com>
2019-05-17 12:23:56 -0600

    Ajustes a Cambio_orden

--- routes.js
+++ routes.js
@@ -25,26 +25,26 @@ router.get('/crear_orden/login', (req, res) => {
 router.post("/crear_orden", (req, res) => {
   numeroEmpleado = req.body.user;
 
-    db.query(`SELECT COUNT( * ) AS count FROM empleados WHERE Gafete=${numeroEmpleado}`, function (err, count, fields) {     
+  db.query(`SELECT COUNT( * ) AS count FROM empleados WHERE Gafete=${numeroEmpleado}`, function (err, count, fields) {
     if (err) {
       res.render('index.ejs')
-    }else{
-      
-    if(count[0].count==0){
-      res.render('index.ejs')
-    }
-    else{
+    } else {
 
-    db.query(`SELECT Nombre FROM empleados WHERE Gafete=${numeroEmpleado}`, function (err, result3, fields) {     
-      if (err) throw err;
-      db.query("SELECT * FROM departamento", function (err, result1, fields) {
-        if (err) throw err;
-        db.query("SELECT * FROM maquinas", function (err, result2, fields) {
+      if (count[0].count == 0) {
+        res.render('index.ejs')
+      }
+      else {
+
+        db.query(`SELECT Nombre FROM empleados WHERE Gafete=${numeroEmpleado}`, function (err, result3, fields) {
           if (err) throw err;
+          db.query("SELECT * FROM departamento", function (err, result1, fields) {
+            if (err) throw err;
+            db.query("SELECT * FROM maquinas", function (err, result2, fields) {
+              if (err) throw err;
 
-            res.render('crear_orden.ejs', {
-              data: result1, data2: result2, data3: result3[0].Nombre, data4: numeroEmpleado
-              });         
+              res.render('crear_orden.ejs', {
+                data: result1, data2: result2, data3: result3[0].Nombre, data4: numeroEmpleado
+              });
             });
           });
         });
@@ -108,62 +108,114 @@ router.post('/guardar_orden', (req, res) => {
 
 
 router.get('/ordenes', (req, res) => {
-    db.query(`SELECT * FROM ordenes, departamento, areas_componentes_afectados 
+  db.query(`SELECT * FROM ordenes, departamento, areas_componentes_afectados 
     WHERE (ordenes.departamento = departamento.id_departamento) 
     AND(ordenes.parte_afectada= areas_componentes_afectados.id_componente) ORDER BY id_orden DESC`, function (err, result2, fields) {
-        if (err) throw err;
-        res.render('ordenes.ejs', {
-          data: result2
-        });
+      if (err) throw err;
+      res.render('ordenes.ejs', {
+        data: result2
       });
-  });
+    });
+});
 
 
 router.post("/cerrar_orden", (req, res) => {
-    numeroEmpleado = req.body.user;
+  numeroEmpleado = req.body.user;
 
-    db.query(`SELECT COUNT( * ) AS count FROM empleados WHERE Gafete=${numeroEmpleado}`, function (err, count, fields) {     
-      if (err) {
-        res.render('index.ejs')
-      }else{
-  
-      if(count[0].count==0){
-        res.render('index.ejs')
-      }else{
+  db.query(`SELECT COUNT( * ) AS count FROM empleados WHERE Gafete=${numeroEmpleado}`, function (err, count, fields) {
+    if (err) {
+      res.render('index.ejs')
+    } else {
 
-    db.query(`SELECT Nombre FROM empleados WHERE Gafete=${numeroEmpleado}`, function (err, result3, fields) {     
-      if (err) throw err;
-      
-      nombreEmpleado=result3[0].Nombre;
+      if (count[0].count == 0) {
+        res.render('index.ejs')
+      } else {
+
+        db.query(`SELECT Nombre FROM empleados WHERE Gafete=${numeroEmpleado}`, function (err, result3, fields) {
+          if (err) throw err;
+
+          nombreEmpleado = result3[0].Nombre;
 
           res.render('cerrar_orden.ejs', {
             data: numeroEmpleado, data2: nombreEmpleado
-          });      
-        });      
+          });
+        });
       }
     }
   });
 });
 
 
-  router.post("/cerrar_orden2", (req, res) => {
+router.post("/cerrar_orden2", (req, res) => {
 
-    numeroEmpleado = req.body.numeroEmpleado;
-    nombreEmpleado = req.body.nombreEmpleado;
-    id_orden = req.body.id_orden;
+  numeroEmpleado = req.body.numeroEmpleado;
+  nombreEmpleado = req.body.nombreEmpleado;
+  id_orden = req.body.id_orden;
 
-    db.query(`SELECT * FROM ordenes,areas_componentes_afectados 
+  db.query(`SELECT * FROM ordenes,areas_componentes_afectados 
               WHERE ordenes.id_orden = ${id_orden}
-              AND ordenes.parte_afectada = areas_componentes_afectados.id_componente`, function (err, result1, fields) {  
-                       
-      parteAfectada = result1[0].componente           
+              AND ordenes.parte_afectada = areas_componentes_afectados.id_componente`, function (err, result1, fields) {
+
+      parteAfectada = result1[0].componente
       res.render('cerrar_orden2.ejs', {
-        data: {numeroEmpleado, nombreEmpleado,id_orden,parteAfectada}
-        //
+        data: { numeroEmpleado, nombreEmpleado, id_orden, parteAfectada }
+      });
     });
-  });
 });
 
+
+router.post('/cambio_orden/:id', (req, res) => {
+  accionTomada = req.params.id;
+  nombreEmpleado = req.body.nombreEmpleado;
+  numeroEmpleado = req.body.numeroEmpleado;
+  id_orden = req.body.id_orden;
+  current_date = req.body.current_date; //Fecha sin formato
+  formatted_current_date = req.body.formatted_current_date; //Fecha con formato YYYY-MM-DD hh:mm:ss
+  clave_cierre = req.body.clave_cierre;
+  parteAfectada = req.body.parteAfectada;
+  actividades = req.body.actividades;
+  
+
+  db.query(`SELECT * FROM ordenes WHERE id_orden = ${id_orden}`,function (err,result,fields){
+
+    ordenFecha = result[0].fecha_hora;
+    var startDate = new Date(ordenFecha);//Fecha en que se creo la orden de trabajo
+    var endDate   = new Date(current_date);//Fecha en la que se esta tendiendo la orden de trabajo, viene de cerrar_orden2(current_date)
+    var seconds = (endDate.getTime() - startDate.getTime()) / 1000;
+    
+  if (accionTomada == "atendida") {
+    db.query(`UPDATE ordenes SET 
+            status= "${accionTomada}",
+            acciones_atendida= "${actividades}" ,
+            fecha_hora_atendida= "${formatted_current_date}" ,
+            tiempo_atendida= "${seconds}",
+            usuario_atendida= "${nombreEmpleado}" 
+            WHERE id_orden = ${id_orden}`, function (err, result, fields) {
+                 if (err) throw err;
+
+        res.render('cambio_orden.ejs', {
+          data: { accionTomada, nombreEmpleado, numeroEmpleado, id_orden, formatted_current_date, clave_cierre, parteAfectada, actividades }
+        });
+      });
+
+  } else {
+    db.query(`UPDATE ordenes SET 
+              status= "${accionTomada}",
+              fecha_hora_cierre= "${formatted_current_date}",
+              usuario_cierre= "${nombreEmpleado}",
+              acciones_cierre= "${actividades}",
+              tiempo_muerto= "${seconds}",
+              area_real_afectada= "NULL",
+              parte_real_afectada= "NULL"
+              WHERE id_orden = ${id_orden}`, function (err, result1, fields) {
+        res.render('cambio_orden.ejs', {
+          data: { accionTomada, nombreEmpleado, numeroEmpleado, id_orden, formatted_current_date, clave_cierre, parteAfectada, actividades }
+        });
+      });
+
+  }
+})
+});
 
 router.get('*', (req, res) => {
   res.send('404 Page not found');
