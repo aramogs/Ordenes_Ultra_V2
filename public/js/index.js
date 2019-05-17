5d5793bf875ed1515016a59e189d3e36958b75c0
aramogs <aramogs@users.noreply.github.com>
2019-05-17 17:34:39 -0600

    Paginacion en ordenes

--- public/js/index.js
+++ public/js/index.js
@@ -1,33 +1,53 @@
-$(document).ready(function(){
-    $(".submenu > a").click(function(e) {
-      e.preventDefault();
-      var $li = $(this).parent("li");
-      var $ul = $(this).next("ul");
-  
-      if($li.hasClass("open")) {
-        $ul.slideUp(350);
-        $li.removeClass("open");
-      } else {
-        $(".nav > li > ul").slideUp(350);
-        $(".nav > li").removeClass("open");
-        $ul.slideDown(350);
-        $li.addClass("open");
-      }
-    });
+
+$("#menu-toggle").click(function (e) {
+  e.preventDefault();
+  $("#wrapper").toggleClass("toggled");
+});
+
+
+
+
+
+function pagination() {$( "#target" ).keyup(function() {
+  alert( "Handler for .keyup() called." );
+});
+  table = $('.table_id').length;
+  ordenes = $('.table_roww')
+  pag =  $('.pagination')
+
+  pages = Math.ceil(table / 20); 
+  for (let i = 1; i < pages; i++) {
+  pag.append(`
+  <li class="page-item">
+  <a class="page-link" href="#">${i}</a>
+  </li>`)
     
-  });
+  }
+  for (let i = 0; i < ordenes.length; i++) {
+    if(i < 20){
+      ordenes[i].style.display=""
+    }else{
+      ordenes[i].style.display="none"
+    }
+    
+  }
+  
+  $( ".page-item" ).click(function(e) {
+    e.preventDefault;
+    let btnNumber = parseInt(e.target.textContent);
 
-  $("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-  });
+    let top = btnNumber*20;
+    let bottom = top - 20;
 
-function getDate() {
-  var today = new Date();
-  var dd = String(today.getDate()).padStart(2, '0');
-  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-  var yyyy = today.getFullYear();
-  today = mm + '-' + dd + '-' + yyyy;
-  return today;
+    console.log(table)
+    for (let i = 0; i < ordenes.length; i++) {
+      if( i >=bottom && i<top){
+        ordenes[i].style.display=''
+      }else{
+        ordenes[i].style.display='none'
+      }
+      
+    }
+  });
 }
- 
+
