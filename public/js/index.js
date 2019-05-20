90087c9742b3783760f4a21729ee80c491e0c768
ciscomar <47339599+ciscomar@users.noreply.github.com>
2019-05-20 09:18:11 -0600

    cambios bootstrap

--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,10 @@ $( document ).ready(function() {
           $('#myTable').dataTable( {
             "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]]
           } );
+
+          if($('#status_cerrar2').text()=='atendida'){
+          $('#atender').hide()
+          }
       
 
 });
@@ -17,28 +21,43 @@ $("#menu-toggle").click(function (e) {
 
 //Revisar Clave de Cierre
   var txtclave= document.getElementById("clave_cierre");
-  var clave = document.getElementById("clave").textContent;
+  var clave = document.getElementById("clave_cerrar2").textContent;
   var btnAtender = document.getElementById("atender");
   var btnCerrar = document.getElementById("cerrar");
+  var lblclave= document.getElementById('lbl_clave_cerrar2')
+  var msg=document.getElementById("msg");
   txtclave.classList.add('border-danger');
+  lblclave.classList.add('text-danger');
+  msg.classList.add('text-danger');
 
   txtclave.addEventListener("keyup",function(e){
     if(txtclave.value==clave){
+      lblclave.classList.remove('text-danger');
+      msg.classList.remove('text-danger');
       $(btnAtender).prop("disabled",false);
       $(btnCerrar).prop("disabled",false);
       txtclave.classList.remove('border-danger');
       txtclave.classList.add('border-success');
       txtclave.classList.add('alert-success');
+      lblclave.classList.add('text-success');
+      msg.classList.add('text-success');
 
-      document.getElementById("msg").innerHTML='Correcta';
+      msg.innerHTML='Correcta';
     }else{
+      lblclave.classList.remove('text-success');
+      msg.classList.remove('text-success');
       $(btnAtender).prop("disabled",true);
       $(btnCerrar).prop("disabled",true);
       txtclave.classList.remove('alert-success');
       txtclave.classList.add('border-danger');
-      document.getElementById("msg").innerHTML='Incorrecta';
+      msg.innerHTML='Incorrecta';
+      lblclave.classList.remove('text-success');
+      msg.classList.remove('text-success');
+      lblclave.classList.add('text-danger');
+      msg.classList.add('text-danger');
 
     }
 
   });
+
 
--- routes.js
+++ routes.js
@@ -4,11 +4,7 @@ const routesController = require('./routesController')
 
 //Routes
 
-router.get('/', (req, res) => {
-  res.render('index.ejs');
-});
-
-
+router.get('/', routesController.index_GET);
 router.get('/login/:id', routesController.login);
 router.get('/crear_orden/login', routesController.crear_orden_GET);
 router.post('/crear_orden', routesController.crear_orden_POST);
