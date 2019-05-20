132d0a74c52801df4e66c253d70a66be0ec3b009
aramogs <aramogs@users.noreply.github.com>
2019-05-20 09:19:12 -0600

    Cabmbio de tarjetas nuevo archivo

--- routesController.js
+++ routesController.js
@@ -248,14 +248,34 @@ controller.historial_POST = (req, res) => {
 
     db.query(`SELECT * FROM ordenes, departamento, areas_componentes_afectados 
       WHERE (ordenes.departamento = departamento.id_departamento) 
-      AND(ordenes.parte_afectada= areas_componentes_afectados.id_componente) AND (ordenes.reporto ="${numeroEmpleado}") ORDER BY id_orden DESC `, function (err, result2, fields) {
+      AND(ordenes.parte_afectada= areas_componentes_afectados.id_componente) AND (ordenes.reporto ="${numeroEmpleado}") ORDER BY id_orden DESC `, function (err, result, fields) {
             if (err) throw err;
 
-            res.render('historial.ejs', {
-                data: result2, data2: numeroEmpleado
+            db.query(`SELECT COUNT(*) AS abiertas FROM ordenes WHERE status ="Abierta"`, function (err, result2, fields) {
+                if (err) throw err;
+
+
+                db.query(`SELECT COUNT(*) AS atendidas FROM ordenes WHERE status ="atendida"`, function (err, result3, fields) {
+                    if (err) throw err;
+
+
+                    db.query(`SELECT COUNT(*) AS cerradas FROM ordenes WHERE status ="cerrada"`, function (err, result4, fields) {
+                        if (err) throw err;
+
+                        ordenesAbiertas = result2[0].abiertas
+                        ordenesAtendidas = result3[0].atendidas
+                        ordenesCerradas = result4[0].cerradas
+
+
+                        res.render('historial.ejs', {
+                            data: result, data2: { ordenesAbiertas, ordenesAtendidas, ordenesCerradas }, data3: numeroEmpleado
+                        });
+                    });
+                });
             });
         });
 };
+
 
 controller.revisar_POST = (req, res) => {
     test = req.params.id
